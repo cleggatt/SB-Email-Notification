20a3884574bc5ecc25ae91f0902296dd911a7d08
cleggatt <cleggatt@users.noreply.github.com>
2014-03-01 11:41:07 +1100

    Refactor to tidy up code.

--- notify.py
+++ notify.py
@@ -24,8 +24,6 @@
 #
 # Note that this script makes no allowance for concurrent invocations.
 
-TVDB_API_URL = 'http://thetvdb.com/api/'
-
 import ConfigParser
 import httplib2
 import os
@@ -34,69 +32,92 @@ import sys
 import time
 import xml.etree.ElementTree as ElementTree
 
-scriptLocation = os.path.dirname(os.path.realpath(__file__))
+SCRIPT_LOCATION = os.path.dirname(os.path.realpath(__file__))
 
-lastRunFile = scriptLocation + '/lastrun.dat'
 
-if os.path.isfile(lastRunFile):
-    f = open(lastRunFile, 'r+')
-    lastRun = float(f.readline())
-    f.seek(0)
-    f.truncate()
-else:
-    lastRun = 0
-    f = open(lastRunFile, 'w')
+def last_run_millis():
+    last_run_file = SCRIPT_LOCATION + '/lastrun.dat'
 
-timeNow = time.time()
-f.write('{}'.format(timeNow))
-f.close()
+    if os.path.isfile(last_run_file):
+        f = open(last_run_file, 'r+')
+        last_run = float(f.readline())
+        f.seek(0)
+        f.truncate()
+    else:
+        last_run = 0
+        f = open(last_run_file, 'w')
 
-fileChangeTime = os.path.getmtime(sys.argv[1])
+    now = time.time()
+    f.write('{}'.format(now))
+    f.close()
 
-if fileChangeTime < lastRun:
+    return last_run
+
+
+def file_changed_millis(path):
+    return os.path.getmtime(path)
+
+
+def parse_config():
+    config_file = SCRIPT_LOCATION + '/notify.ini'
+    config = ConfigParser.ConfigParser()
+    config.read(config_file)
+    return config
+
+
+def series_name(config, id):
+
+    api_url = 'http://thetvdb.com/api/'
+    api_key = config.get('TVDB', 'tvdb_api_key')
+
+    url = api_url + api_key + '/series/' + id
+
+    h = httplib2.Http(SCRIPT_LOCATION + '/.cache')
+    (resp, content) = h.request(url, "GET")
+
+    root = ElementTree.fromstring(content)
+    return root.find('./Series/SeriesName').text
+
+
+def notify(config, series, season, episode):
+
+    mail_server = config.get('Mail', 'mail_server')
+    mail_port = config.get('Mail', 'mail_port')
+    mail_account = config.get('Mail', 'mail_account')
+    mail_password = config.get('Mail', 'mail_password')
+
+    mail_to = config.get('Notification', 'mail_to')
+    mail_from = config.get('Notification', 'mail_from')
+
+    subject = 'New episode of ' + series
+    body = 'A new episode of ' + seriesName + ' is available (Season ' + season + ', episode ' + episode + ')'
+
+    headers = ["From: " + mail_from,
+               "Subject: " + subject,
+               "To: " + mail_to,
+               "MIME-Version: 1.0",
+               "Content-Type: text/html"]
+    headers = "\r\n".join(headers)
+
+    session = smtplib.SMTP(mail_server, mail_port)
+    session.ehlo()
+    session.starttls()
+    session.login(mail_account, mail_password)
+    session.sendmail(mail_from, mail_to, headers + "\r\n\r\n" + body)
+    session.quit()
+
+
+last_run = last_run_millis()
+file_changed = file_changed_millis(sys.argv[1])
+if file_changed < last_run:
     sys.exit()
 
-configFile = scriptLocation + '/notify.ini'
-Config = ConfigParser.ConfigParser()
-Config.read(configFile)
+config = parse_config()
 
-TVDB_API_KEY = Config.get('TVDB', 'tvdb_api_key')
+series_id = sys.argv[3]
+season_num = sys.argv[4]
+episode_num = sys.argv[5]
 
-MAIL_SERVER = Config.get('Mail', 'mail_server')
-MAIL_PORT = Config.get('Mail', 'mail_port')
-MAIL_ACCOUNT = Config.get('Mail', 'mail_account')
-MAIL_PASSWORD = Config.get('Mail', 'mail_password')
+seriesName = series_name(config, series_id)
 
-MAIL_TO = Config.get('Notification', 'mail_to')
-MAIL_FROM = Config.get('Notification', 'mail_from')
-
-seriesId = sys.argv[3]
-season = sys.argv[4]
-episode = sys.argv[5]
-
-dataUrl = TVDB_API_URL + TVDB_API_KEY + '/series/' + seriesId
-
-h = httplib2.Http(scriptLocation + '/.cache')
-(resp, content) = h.request(dataUrl, "GET")
-
-root = ElementTree.fromstring(content)
-seriesName = root.find('./Series/SeriesName').text
-
-sender = MAIL_FROM
-recipient = MAIL_TO
-subject = 'New episode of ' + seriesName
-body = 'A new episode of ' + seriesName + ' is available (Season ' + season + ', episode ' + episode + ')'
-
-headers = ["From: " + sender,
-           "Subject: " + subject,
-           "To: " + recipient,
-           "MIME-Version: 1.0",
-           "Content-Type: text/html"]
-headers = "\r\n".join(headers)
-
-session = smtplib.SMTP(MAIL_SERVER, MAIL_PORT)
-session.ehlo()
-session.starttls()
-session.login(MAIL_ACCOUNT, MAIL_PASSWORD)
-session.sendmail(sender, recipient, headers + "\r\n\r\n" + body)
-session.quit()
+notify(config, seriesName, season_num, episode_num)
